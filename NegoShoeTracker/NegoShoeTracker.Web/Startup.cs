57b3b5cbee14a029cd2c6247e732c04cb4977c1e
meanprogrammer <mean.programmer.d@gmail.com>
2016-05-11 13:49:55 +0800

    web

---
+++ NegoShoeTracker/NegoShoeTracker.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NegoShoeTracker.Web.Startup))]
+namespace NegoShoeTracker.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
