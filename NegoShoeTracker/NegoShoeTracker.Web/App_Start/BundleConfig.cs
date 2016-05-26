0bafd0bf90021ce7d257ad13cb6253cc20973ed1
meanprogrammer <mean.programmer.d@gmail.com>
2016-05-26 15:30:28 +0800

    date

--- NegoShoeTracker/NegoShoeTracker.Web/App_Start/BundleConfig.cs
+++ NegoShoeTracker/NegoShoeTracker.Web/App_Start/BundleConfig.cs
@@ -21,11 +21,15 @@ namespace NegoShoeTracker.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-datepicker.js",
+                      "~/Scripts/DatePickerReady.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                       "~/Content/bootstrap-datepicker3.css",
                       "~/Content/site.css"));
+
         }
     }
 }
---
+++ NegoShoeTracker/NegoShoeTracker.Web/Scripts/DatePickerReady.js
@@ -0,0 +1,8 @@
+﻿if (!Modernizr.inputtypes.date) {
+    $(function () {
+
+        $(".datecontrol").datepicker().on('changeDate', function (e) {
+            $('.datecontrol').datepicker('hide');
+        });
+    });
+}
